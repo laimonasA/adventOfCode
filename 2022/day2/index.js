ca819c67393944aced31a102b756168c594b510d
laimonasA <laimonas.andriejauskas@gmail.com>
2023-01-23 15:48:20 +0000

    Day 2 small fixup.

--- 2022/day2/index.js
+++ 2022/day2/index.js
@@ -1,17 +1,15 @@
 import * as fs from "fs";
 
-const itemScore = { X: 1, Y: 2, Z: 3 };
-
 const roundScore = {
-  "A X": 3,
-  "A Y": 6,
-  "A Z": 0,
-  "B X": 0,
-  "B Y": 3,
-  "B Z": 6,
-  "C X": 6,
-  "C Y": 0,
-  "C Z": 3,
+  "A X": 4,
+  "A Y": 8,
+  "A Z": 3,
+  "B X": 1,
+  "B Y": 5,
+  "B Z": 9,
+  "C X": 7,
+  "C Y": 2,
+  "C Z": 6,
 };
 
 const roundScoreOutcome = {
@@ -30,11 +28,7 @@ const run = () => {
   const dataArray = fs.readFileSync("./input.txt", "utf-8").split("\n");
   const data = dataArray.slice(0, dataArray.length - 1);
 
-  const score1 = data.reduce(
-    (acc, cur) => acc + roundScore[cur] + itemScore[cur[2]],
-    0
-  );
-
+  const score1 = data.reduce((acc, cur) => acc + roundScore[cur], 0);
   const score2 = data.reduce((acc, cur) => acc + roundScoreOutcome[cur], 0);
 
   console.log("Part 1 score: ", score1);
